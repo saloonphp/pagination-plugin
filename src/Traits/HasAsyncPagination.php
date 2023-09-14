8747cc69d86da56eb76f8b67f2e60feab9531520
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-09-14 20:04:14 +0100

    Fixed phpstan issues

--- src/Traits/HasAsyncPagination.php
+++ src/Traits/HasAsyncPagination.php
@@ -31,7 +31,7 @@ trait HasAsyncPagination
      */
     public function pool(callable|int $concurrency = 5, ?callable $responseHandler = null, ?callable $exceptionHandler = null): Pool
     {
-        /** @var iterable<mixed, \Saloon\Contracts\Request> $iterator */
+        /** @var iterable<mixed, \Saloon\Http\Request> $iterator */
         $iterator = clone $this->async();
 
         return $this->connector->pool($iterator, $concurrency, $responseHandler, $exceptionHandler);
