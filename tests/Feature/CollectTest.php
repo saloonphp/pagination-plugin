8734ac39d9f86110a23adaa395846ec09417f7b5
jp <jonathan.purvis@visualsoft.co.uk>
2026-03-08 11:52:00 +0000

    Fix | Reset body checksums on rewind to fix false infinite-loop (#17)
    reset body checksums on rewind to fix false infinite-loop on multiple collect()

--- tests/Feature/CollectTest.php
+++ tests/Feature/CollectTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Saloon\Http\Response;
 use Illuminate\Support\Collection;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
 use Saloon\PaginationPlugin\Tests\Fixtures\Connectors\PagedConnector;
 use Saloon\PaginationPlugin\Tests\Fixtures\Requests\SuperheroPagedRequest;
 
@@ -28,4 +30,39 @@ test('can collect through paginated responses and not items', function () {
     expect(toIds($collection[3]->json('data')))->toEqual([16, 17, 18, 19, 20]);
 
     expect($paginator->getTotalResults())->toEqual(20);
+});
+
+/**
+ * @see https://github.com/saloonphp/saloon/issues/464
+ */
+test('multiple collect calls on same LazyCollection do not trigger infinite loop detection', function () {
+    $singlePageResponse = [
+        'data' => [['id' => 1], ['id' => 2]],
+        'next_page_url' => null,
+    ];
+
+    $connector = new PagedConnector;
+    $connector->withMockClient(new MockClient([
+        MockResponse::make($singlePageResponse),
+        MockResponse::make($singlePageResponse),
+        MockResponse::make($singlePageResponse),
+        MockResponse::make($singlePageResponse),
+        MockResponse::make($singlePageResponse),
+        MockResponse::make($singlePageResponse),
+    ]));
+
+    $request = new SuperheroPagedRequest;
+    $lazyCollection = $connector->paginate($request)->collect();
+
+    $first = $lazyCollection->collect();
+    expect($first)->toBeInstanceOf(Collection::class);
+    expect($first->pluck('id')->all())->toBe([1, 2]);
+
+    $lazyCollection->collect();
+    $lazyCollection->collect();
+    $lazyCollection->collect();
+    $lazyCollection->collect();
+
+    $again = $lazyCollection->collect();
+    expect($again->pluck('id')->all())->toBe([1, 2]);
 });
